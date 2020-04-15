506623fe0dc4f628bfe87502bbd263a3135af8cb
Tuomas Katajisto <t@ktj.st>
2020-04-15 18:15:33 +0300

    Beatified code and added run on enter key.

--- main.js
+++ main.js
@@ -7,6 +7,14 @@
 
 */
 
+document.addEventListener('keypress', enterListener);
+
+function enterListener(e) {
+    if (e.keyCode == 13) {
+        reset();
+    }
+}
+
 var collisionCount = 0
 var x1 = 300
 var x2 = 100
@@ -15,7 +23,7 @@ var v2 = 0
 var m1 = Number(document.getElementById('weight1').value)
 var m2 = Number(document.getElementById('weight2').value)
 var delay = Number(document.getElementById('delay').value)
-console.log(m1,m2)
+console.log(m1, m2)
 
 var canvas = document.getElementById('canvas'),
     cw = canvas.width,
@@ -40,46 +48,46 @@ function sleep(ms) {
 
 function loop() {
     let tickTime = 0
-    while(1) {
-	if(v2 < 0) {
-	    let t = Math.abs((x2/v2))
-	    if(tickTime + t >= delay) {
-		let delta = delay - tickTime
-		x2 += v2*delta
-		x1 += v1*delta
-		break
-	    } else {
-		collisionCount++
-		tickTime += t
-		x2 = 0
-		x1 += v1*t
-		v2 *= -1
-		
-	    }
-	}
+    while (1) {
+        if (v2 < 0) {
+            let t = Math.abs((x2 / v2))
+            if (tickTime + t >= delay) {
+                let delta = delay - tickTime
+                x2 += v2 * delta
+                x1 += v1 * delta
+                break
+            } else {
+                collisionCount++
+                tickTime += t
+                x2 = 0
+                x1 += v1 * t
+                v2 *= -1
 
-	let n = ((x2+100)-x1)/(v1-v2)
+            }
+        }
 
-	if(tickTime + n >= delay || v2 < v1) {
-	    let delta = delay - tickTime
-	    x2 += v2*delta
-	    x1 += v1*delta
-	    break
-	} else {
-	    collisionCount++
-	    let dvv = (-2.0*(m1)*(v1-v2))/(m1+m2)
-	    v2 -= dvv
-	    v1 -= -1*(dvv*(m2/m1))
+        let n = ((x2 + 100) - x1) / (v1 - v2)
 
-	    x1 += v1*n
-	    x2 += v2*n
-	    tickTime += n
-	}
+        if (tickTime + n >= delay || v2 < v1) {
+            let delta = delay - tickTime
+            x2 += v2 * delta
+            x1 += v1 * delta
+            break
+        } else {
+            collisionCount++
+            let dvv = (-2.0 * (m1) * (v1 - v2)) / (m1 + m2)
+            v2 -= dvv
+            v1 -= -1 * (dvv * (m2 / m1))
+
+            x1 += v1 * n
+            x2 += v2 * n
+            tickTime += n
+        }
     }
-    
-    
+
+
     cx.fillStyle = "#FFFFFF"
-    cx.fillRect(0,0,cw,ch)
+    cx.fillRect(0, 0, cw, ch)
     cx.fillStyle = "#ffc2ef";
     cx.fillRect(x2, 400, 100, 100);
     cx.fillStyle = "#000000";
@@ -88,7 +96,7 @@ function loop() {
     cx.fillStyle = "#000000";
     cx.font = "30px Arial";
     cx.fillText("COLLISION COUNT: " + collisionCount, 10, 30);
-    
+
 }
 
 
